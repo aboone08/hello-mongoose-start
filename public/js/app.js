36c812efab6d10b031aa2135bd9cc2dec83e3329
aboone08 <andranique.boone@gmail.com>
2016-03-10 20:57:54 -0500

    more stuff happened

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,27 @@
 angular.module('todoApp') //this is the getter syntax, we can use this so we don't have to make a variable to store our angular application; YAYYY no globals
       .controller('TodoController', TodoController);
 
+
+
+
+
 TodoController.$inject = ['$scope', '$http'];
 
 function TodoController($scope, $http){
   $scope.todos = [];
-
   initTodos();
 
+// Create a new todo
+$scope.saveTodo = function(){
+  $http.post('/api/todos', $scope.newTodo)
+    .then(function(response){
+      initTodos();
+      $scope.newTodo = {};
+    })
+    .catch(function(err){
+      console.err(err);
+    });
+}
 
   function initTodos(){
     $http.get('/api/todos')
