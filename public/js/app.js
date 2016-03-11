3d558bfb4ab129ca069002c08e0012d778bfe7d8
aboone08 <andranique.boone@gmail.com>
2016-03-10 21:05:02 -0500

    fixed conflicts, up to date

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,7 @@
+angular.module('todoApp', [
+
+]); //this is the setter syntax, you are creating an application using this one
+
 angular.module('todoApp') //this is the getter syntax, we can use this so we don't have to make a variable to store our angular application; YAYYY no globals
       .controller('TodoController', TodoController);
 
