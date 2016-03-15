4d7348be401d0f28bba3b2aa2b68a42cb276be96
aboone08 <andranique.boone@gmail.com>
2016-03-14 21:04:21 -0400

    finished refactoring session

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var express = require('express'),
     Schema = mongoose.Schema,
     bodyParser = require('body-parser'),
     server  = express(),
-    Todo = require('./models/todo.model.js');
+    Todo = require('./models/todo.model.js'),
+    TodoCtrl = require('./controllers/todo.controller.js');
 
 //create a connection to our db
 mongoose.connect('mongodb://localhost/todoApp');
@@ -22,50 +23,10 @@ server.get('/', function(req, res){
   res.sendFile('public/html/index.html', {root: __dirname});
 });
 
-server.get('/api/todos', function(req, res){
-  Todo.find(function(err, todos){
-    if(err) throw err;
-
-    res.json(todos);
-  });
-});
-
-server.post('/api/todos', function(req,res){
-
-  var desc = req.body.desc;
-  var todoObj = {
-    desc: desc,
-    completed: false
-  };
-  Todo.create(todoObj, function(err, todo){
-    if(err) throw err;
-
-    res.json(todo);
-  });
-});
-
-server.put('/api/todos/:id', function(req, res){
-  var id = req.params.id;
-  var desc = req.body.desc;
-  var completed = req.body.completed;
-  var update = {
-    desc: desc,
-    completed: completed
-  };
-
-  Todo.findOneAndUpdate({_id: id}, update, {new: true}, function(err, todo){
-    if(err) throw err;
-
-    res.json(todo);
-  });
-});
-
-server.delete('/api/todos/:id', function(req, res){
-  Todo.findOneAndRemove({_id: req.params.id}, function(err, todo){
-    if(err) throw err;
-    res.json(todo);
-  });
-});
+server.get('/api/todos', TodoCtrl.getAll);
+server.post('/api/todos', TodoCtrl.create);
+server.put('/api/todos/:id', TodoCtrl.update);
+server.delete('/api/todos/:id', TodoCtrl.delete);
 
 server.listen(port, function(){
   console.log('Now listening on port ' + port);
